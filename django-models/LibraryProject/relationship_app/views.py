6a0a0a2cd22f29e8babfcb7e325839414ffd03a1
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-20 23:22:51 +0100

    fix: use correct path to list_books.html in render()

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
-
 from .models import Book, Library
 
-# Function-based view: returns plain text list of books and authors
+# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    response_text = "Books Available:\n"
-    for book in books:
-        response_text += f"- {book.title} by {book.author.name}\n"
-    return HttpResponse(response_text, content_type='text/plain')
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view: shows library details and books in HTML template
+# Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
