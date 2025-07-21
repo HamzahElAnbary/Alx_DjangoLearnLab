f8acd35987f089924e4a15d9cd465de00aebef89
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-21 14:40:23 +0100

    fix: add missing import for UserCreationForm in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,43 @@
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+# relationship_app/views.py
+
 from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
-def user_login(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('list_books')  # Redirect to home or books
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
 
-def user_logout(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-def register(request):
-    if request.method == "POST":
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+
+# User Registration View
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+    def post(self, request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+        return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# User Login View (uses Django's built-in view)
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+
+
+# User Logout View (uses Django's built-in view)
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
