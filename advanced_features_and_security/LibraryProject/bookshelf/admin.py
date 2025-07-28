b92720babde0a361a06ca9bf6ff669f8554067ac
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-28 12:46:22 +0100

    Register CustomUser with CustomUserAdmin in admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    # You can customize the admin interface here if needed
+    # For example, add your custom fields to the fieldsets or add_fieldsets
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
