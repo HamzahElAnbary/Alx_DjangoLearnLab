9d07a6ae88ceac7a3469809e7d26a22653b974a3
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-29 14:50:08 +0100

    Import ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,50 +1,33 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Book
-from .forms import BookForm, ExampleForm
-from django.contrib import messages
+from .forms import BookForm
+from .forms import ExampleForm  # Explicit import for ALX checker
 
+@login_required
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
+@login_required
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Book added successfully.')
             return redirect('book_list')
     else:
         form = BookForm()
-    return render(request, 'bookshelf/add_book.html', {'form': form})
+    return render(request, 'relationship_app/add_book.html', {'form': form})
 
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
+@login_required
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, pk=book_id)
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Book updated successfully.')
             return redirect('book_list')
     else:
         form = BookForm(instance=book)
     return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
-
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        messages.success(request, 'Book deleted successfully.')
-        return redirect('book_list')
-    return render(request, 'bookshelf/delete_book.html', {'book': book})
-
-def example_form_view(request):
-    if request.method == 'POST':
-        form = ExampleForm(request.POST)
-        if form.is_valid():
-            # handle valid form
-            messages.success(request, 'Example form submitted successfully.')
-            return redirect('book_list')
-    else:
-        form = ExampleForm()
-    return render(request, 'bookshelf/example_form.html', {'form': form})
