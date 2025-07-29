6544b40de071de5803bba46dee7ce4c989062ad7
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-29 14:45:52 +0100

    Import ExampleForm in views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,50 +1,50 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm, ExampleForm
+from django.contrib import messages
 
 def book_list(request):
-    """
-    Display a list of all books.
-    """
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-
 def add_book(request):
-    """
-    Handle creation of a new book.
-    """
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Book added successfully.')
             return redirect('book_list')
     else:
         form = BookForm()
     return render(request, 'bookshelf/add_book.html', {'form': form})
 
-
 def edit_book(request, pk):
-    """
-    Edit an existing book by its primary key.
-    """
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Book updated successfully.')
             return redirect('book_list')
     else:
         form = BookForm(instance=book)
     return render(request, 'bookshelf/edit_book.html', {'form': form, 'book': book})
 
-
 def delete_book(request, pk):
-    """
-    Delete a book by its primary key.
-    """
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         book.delete()
+        messages.success(request, 'Book deleted successfully.')
         return redirect('book_list')
     return render(request, 'bookshelf/delete_book.html', {'book': book})
+
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # handle valid form
+            messages.success(request, 'Example form submitted successfully.')
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
