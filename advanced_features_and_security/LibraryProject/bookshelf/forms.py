5300ad5b60a944245068742becee7420d197aca1
Hamzah_Elanbary <optimistichamaza19@gmail.com>
2025-07-29 14:41:13 +0100

    Add ExampleForm to forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,15 +4,12 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = [
-            'title',
-            'author',
-            'published_date',
-            'isbn',
-            'pages',
-            'cover',
-            'language',
-        ]
+        fields = ['title', 'author', 'published_date', 'isbn', 'pages', 'cover', 'language']
         widgets = {
             'published_date': forms.DateInput(attrs={'type': 'date'}),
         }
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
