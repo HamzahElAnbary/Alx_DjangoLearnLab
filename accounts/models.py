c4bb80dd11f1f07cfefc8f52175021d5938749c8
Hamzah El anbary <126053220+HamzahElAnbary@users.noreply.github.com>
2025-08-25 12:49:59 +0100

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,13 @@
-# accounts/models.py
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-class User(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pics/", blank=True, null=True
+    )
     followers = models.ManyToManyField(
-        'self',
-        symmetrical=False,
-        related_name='following',
-        blank=True
+        "self", symmetrical=False, related_name="following", blank=True
     )
 
     def __str__(self):
